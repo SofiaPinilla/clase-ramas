941969572ff16c6630f828715ed8596ac186f14f
Euralio <davidbootcamp2020@gmail.com>
2023-11-03 10:07:16 +0100

    tito endpoint added

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get("/", (req, res) => {
     res.send("holi")
 })
 
+app.get("/tito", (req, res) => {
+    res.send("holi tito")
+})
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
