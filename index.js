6f1e058f3fb1a1c6c692de9ceb27e8f1d49d57f1
Euralio <davidbootcamp2020@gmail.com>
2023-11-03 09:59:39 +0100

    create get endpoint

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express")
 const app = express()
 const PORT = 8080
 
+app.get("/", (req, res) => {
+    res.send("holi")
+})
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
